7d1f5979d171ec1ab742f91d584dea1fecb2c6ca
Tristan <139055954+CodeLord0@users.noreply.github.com>
2026-02-22 16:08:27 +0000

    Add PAT_1 to fetcher retryer call
    Pass personal access token to fetcher retryer.

--- src/fetchers/top-languages.js
+++ src/fetchers/top-languages.js
@@ -69,7 +69,7 @@ const fetchTopLanguages = async (
     throw new MissingParamError(["username"]);
   }
 
-  const res = await retryer(fetcher, { login: username });
+  const res = await retryer(fetcher, { login: username },process.env.PAT_1);
 
   if (res.data.errors) {
     logger.error(res.data.errors);
